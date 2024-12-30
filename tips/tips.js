8f874fac00d68468f7f1a7e021e45ac22633166d
Guilherme Roesler <guiroesler2@gmail.com>
2024-12-29 22:18:54 -0300

    tips - nth-child-pseudo-class

--- tips/tips.js
+++ tips/tips.js
@@ -2,3 +2,25 @@ const container = document.querySelector('#auto-hide>div')
 container.addEventListener('click', () => {
     container.classList.toggle('open')
 })
+
+function createNthChildPseudoElementDivs() {
+    for (let i = 0; i < 15; i++) {
+        const child = document.createElement("div")
+        child.append(i+1)
+        child.classList.add("nth-child-pseudo-class-item")
+
+        document.querySelector("#nth-child-pseudo-class>div").appendChild(child)
+    }
+}
+
+createNthChildPseudoElementDivs()
+
+let i = 1;
+function changeNthChildPseudoElementDivs() {
+    let element = document.getElementById("nth-child-pseudo-class")
+    element.classList.remove('active1', 'active2', 'active3', 'active4', 'active5', 'active6')
+    element.classList.add('active' + i)
+    i>=6 ? i=1 : i++
+}
+
+setInterval(changeNthChildPseudoElementDivs, 1000)
