2d7152af8f75bc8677cdac355f1317f1a34ef9b7
Guilherme Roesler <guiroesler2@gmail.com>
2025-01-19 00:04:49 -0300

    header

--- tips/src/App.js
+++ tips/src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import { useBlurLoad } from './hooks/useBlurLoad';
 import { Routes, Route } from 'react-router-dom'
 
+import Layout from './Layout'
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage'
+import DarkmodeSwitch from './pages/DarkmodeSwitch';
 
 function App() {
   useBlurLoad()
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/login' element={<LoginPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/darkmode-switch' element={<DarkmodeSwitch />} />
+        <Route path='/teste-header' />
+      </Routes>
+    </Layout>
   );
 }
 
--- tips/src/components/Header.js
+++ tips/src/components/Header.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className='header'>
-
+        <header id='Header'>
+            <a href="/home" id='hTitle'>Tips</a>
+            <nav>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/">Page</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Products</a></li>
+                </ul>
+            </nav>
+            <button>Contact</button>
         </header>
     )
 }
